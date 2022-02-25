3b63c9cfc4d549f2713cba2441e56438c9371ef8
Nishchit <nbhellossss@gmail.com>
2022-02-25 21:09:48 +0545

    first commit

---
+++ Backup/index.js
@@ -0,0 +1,103 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url) res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  let payload = '';
+  req.on('data', chunk => {
+    payload += chunk;
+  });
+
+  req.on('end', () => {
+    let result = { error: 'Unknown request' };
+    if (req.url.endsWith('bid')) {
+      result = bid(payload);
+    } else if (req.url.endsWith('play')) {
+      result = play(payload);
+    }
+
+    res.write(JSON.stringify(result));
+    res.end();
+  });
+});
+
+function bid(payload) {
+  /*
+  Bid is called at the starting phase of the game in callbreak.
+  You will be provided with the following data:
+  {
+    "matchId": "M1",
+    "playerId": "P3",
+    "cards": ["1S", "TS", "8S", "7S", "6S", "4S", "3S", "9H", "6H", "5H", "1C", "1D", "JD"],
+    "context": {
+      "round": 1,
+      "players": {
+        "P3": {
+          "totalPoints": 0,
+          "bid": 0
+        },
+        "P0": {
+          "totalPoints": 0,
+          "bid": 3
+        },
+        "P2": {
+          "totalPoints": 0,
+          "bid": 3
+        },
+        "P1": {
+          "totalPoints": 0,
+          "bid": 3
+        }
+      }
+    }
+  }
+  This is all the data that you will require for the bidding phase.
+  */
+  console.log('Returning bid', payload);
+
+  /*
+  ####################################
+  #     Input your code here.        #
+  ####################################
+  */
+
+  // return should have a single field value which should be an int reprsenting the bid value
+  return {
+    value: Math.floor(Math.random() * 4) + 2,
+  };
+}
+
+function play(payload) {
+  /*
+  Play is called at every hand of the game where the user should throw a card.
+  Request data format:
+  {
+    "matchId": "M1",
+    "playerId": "P1",
+    "cards": [ "QS", "9S", "2S", "KH", "JH", "4H", "JC", "9C", "7C", "6C", "8D", "6D", "3D"],
+    "played": [
+      "2H/0",
+      "8H/0"
+    ]
+  }
+  The played field contins all the cards played this turn in order.
+  */
+  console.log('Returning play', payload);
+
+  /*
+  ####################################
+  #     Input your code here.        #
+  ####################################
+  */
+
+  //  return should have a single field value
+  //  which should be an int reprsenting the index of the card to play
+  //  e.g> {"value": payload.cards.indexOf("QS")}
+  //  to play the card "QS"
+  return {
+    value: 0,
+  };
+}
+
+server.listen(7000);
---
+++ app.py
@@ -0,0 +1,114 @@
+# This contains the basic code that you will need to view the game on the sandbox.
+# You will provide all the moves of all the bots in the sandbox.
+# The sandbox is for testing purposes only so, the API data format can change
+# when qualifiers begin.
+# The sandbox is provided so that you can get familiar with building bots before
+#  the event actually begins.
+
+# To setup: `pip install flask, flask_cors`
+# To run: python app.py
+
+import json
+import Cards
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+bid = 1
+played_cards = []
+
+
+@app.route("/bid", methods=["OPTIONS", "POST"])
+def bid():
+    """
+    Bid is called at the starting phase of the game in call break.
+    You will be provided with the following data:
+    {
+        "matchId": "M1",
+        "playerId": "P3",
+        "cards": ["1S", "TS", "8S", "7S", "6S", "4S", "3S", "9H", "6H", "5H", "1C", "1D", "JD"],
+        "context": {
+            "round": 1,
+            "players": {
+            "P3": {
+                "totalPoints": 0,
+                "bid": 0
+            },
+            "P0": {
+                "totalPoints": 0,
+                "bid": 3
+            },
+            "P2": {
+                "totalPoints": 0,
+                "bid": 3
+            },
+            "P1": {
+                "totalPoints": 0,
+                "bid": 3
+            }
+            }
+        }
+    }
+
+
+
+    This is all the data that you will require for the bidding phase.
+    """
+
+    body = request.get_json()
+    # print(json.dumps(body, indent=2))
+    ####################################
+    bid = 0
+    try:
+        cards = body["cards"]
+        for i in cards:
+            if i in Cards.winners:
+                bid += 1
+        print(bid)
+    except:
+        pass
+    #     Input your code here.        #
+    ####################################
+    # return should have a single field value which should be an int representing the bid value
+    return jsonify({"value": bid})
+
+
+@app.route("/play", methods=["OPTIONS", "POST"])
+def play():
+    """
+    Play is called at every hand of the game where the user should throw a card.
+    Request data format:
+    {
+        "matchId": "M1",
+        "playerId": "P1",
+        "cards": [ "QS", "9S", "2S", "KH", "JH", "4H", "JC", "9C", "7C", "6C", "8D", "6D", "3D"],
+        "played": [
+            "2H/0",
+            "8H/0"
+        ]
+    }
+    The played field contains all the cards played this turn in order.
+    """
+    body = request.get_json()
+    try:
+        played_cards.append(body["played"][-1][0:2])
+    except:
+        pass
+    # print(json.dumps(body))
+    ####################################
+    #     Input your code here.        #
+    ####################################
+
+    # return should have a single field value
+    # which should be an int representing the index of the card to play
+    # e.g> {"value": body.cards.index("QS")}
+    # to play the card "QS"
+    return jsonify({"value": 0})
+
+
+# do not change this port; the sandbox server hits this port on localhost
+app.run(port=7000)
+print(played_cards)
